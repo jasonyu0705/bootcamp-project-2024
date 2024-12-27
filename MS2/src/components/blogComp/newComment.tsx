547a0416702bab38f364f14ace79474e9374bdbb
jasonyu0705 <49047194+jasonyu0705@users.noreply.github.com>
2024-12-26 22:29:02 -0800

    comment issue fixed

--- MS2/src/components/blogComp/newComment.tsx
+++ MS2/src/components/blogComp/newComment.tsx
@@ -50,7 +50,7 @@ export default function NewComment({ slug }: Props) {
     try {
       //                                this may change
       
-      const response = await fetch(`http://localhost:3000/api/blogs/${slug}`, {
+      const response = await fetch(`https://bootcamp-project-2024-zeta.vercel.app/api/blogs/${slug}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
